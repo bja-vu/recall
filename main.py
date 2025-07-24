c27986e865916c4e4e815eec59ebcae386f369e9
bnl <buffybnl@gmail.com>
2025-07-25 09:40:55 +1200

    added small constraints for input to avoid accidental running

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ prompt_input = sys.argv[1:]
 if not prompt_input:
     print("error: no prompt entered.")
     exit()
-
 prompt = " ".join(prompt_input)
-print(prompt)
+if len(prompt_input) < 5:
+    print(f"was your prompt correct? - ({prompt})")
+    confirm_prompt = input("enter y/n: ")
+    if confirm_prompt == "n": # doesn't explicitly need a y to continue
+        exit() 
 
 from llama_cpp import Llama
 model = "Phi-3-mini-4k-instruct-q4.gguf"
@@ -18,6 +21,7 @@ prompt_tune = ( #the formatting is ok, but the tone might need work
         "If the question is programming related, opt for inline code examples instead of code blocks. "
         "You can assume the user has a general grasp on the concepts, and just needs help recalling information. "
         "You are adept at programming, and can use jargon where necessary. "
+        "If the question is not explicitly programming related, do not relate it to programming. "
         )
 res = llm.create_chat_completion(
         messages=[
