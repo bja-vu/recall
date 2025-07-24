85b7f6251b65166c74db9b67435aa23b96c0e4fa
bnl <buffybnl@gmail.com>
2025-07-25 09:30:14 +1200

    added a basic input with args

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+import sys
+prompt_input = sys.argv[1:]
+if not prompt_input:
+    print("error: no prompt entered.")
+    exit()
+
+prompt = " ".join(prompt_input)
+print(prompt)
+
 from llama_cpp import Llama
 model = "Phi-3-mini-4k-instruct-q4.gguf"
 
@@ -10,7 +19,6 @@ prompt_tune = ( #the formatting is ok, but the tone might need work
         "You can assume the user has a general grasp on the concepts, and just needs help recalling information. "
         "You are adept at programming, and can use jargon where necessary. "
         )
-prompt = "how to use dicts in python" #hardcoded, will take an input eventually
 res = llm.create_chat_completion(
         messages=[
             {"role": "user", "content": prompt_tune},
