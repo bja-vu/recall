d83779268813589879da9867af8d09edd84e9842
bnl <buffybnl@gmail.com>
2025-08-28 16:35:00 +1200

    fastapi server/client setup using llama.cpp python bindings

--- client/main.py
+++ client/main.py
@@ -2,11 +2,12 @@ import requests
 
 def main():
     url = "http://127.0.0.1:8000/read"
-    data = {"text": "hello world"}
+    data = {"text": "What is the capital of Portugal?"}
     headers = {"Content-Type": "application/json"}
 
     response = requests.post(url, json=data, headers=headers)
-    print(response.text)
+    print(response.json()["response"])
 
 if __name__ == "__main__":
     main()
+
--- server/main.py
+++ server/main.py
@@ -1,10 +1,16 @@
-from fastapi import FastAPI
-from fastapi import Request
+from fastapi import FastAPI, Request
+from llama_cpp import Llama
+
+llm = Llama(model_path="models/capybarahermes-2.5-mistral-7b.Q4_K_M.gguf", gpu_layers=32)
 
 app = FastAPI()
 
 @app.post("/read")
 async def read(req: Request):
     data = await req.json()
-    text = data.get("text", "")
-    return {"user_sent": text}
+    prompt = data.get("text", "")
+    if not prompt:
+        return {"error":"no prompt entered"}
+
+    output = llm(prompt, max_tokens=200)
+    return {"response": output["choices"][0]["text"]}
