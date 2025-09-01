7aad9f8f7b093278d3292554aca2d2818a5fe898
bnl <buffybnl@gmail.com>
2025-09-01 17:35:25 +1200

    FINALLY cpp server for handling LLM requests

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 ## recall - an offline command-line assistant
 
-fastapi command: fastapi dev server/main.py
+cmake .. -DGGML_CUDA=OFF
+make  -j
 
 server command: ./external/llama.cpp/build/bin/llama-server -m "models/capybarahermes-2.5-mistral-7b.Q4_K_M.gguf"
 
--- server/main.cpp
+++ server/main.cpp
@@ -3,120 +3,97 @@
 #include <iostream>
 #include <vector>
 
-const std::string model_path = "../models/capybarahermes-2.5-mistral-7b.Q4_K_M.gguf";
+const std::string model_path = "../models/capybarahermes-2.5-mistral-7b.Q4_K_M.gguf"; // #TODO get model path from args
 const std::string prompt= "the capital of portugal is";
 const int ngl = 99;
-const int n_predict = 32;
+const int n_predict = 128;
 
-int main() {
+const std::string prompt_tune =     
+	"Always answer in two sentences or under 50 words. No extra explanation. No notes.\n"
+	"Assume the user understands the general topic and needs a quick reminder. Freely use slang and jargon where necessary. ALWAYS answer the question.\n"
+	"If the question refers to something that does not exist or is incorrect, say so. Do not answer untruthfully.\n"
+	"If the question is programming related, be pragmatic with your answers. Opt for code instead of descriptions.\n"
+	"Reply using markdown syntax only. Use one asterisk (*text*) for italics, two asterisks (**text**) for bold, and backticks (`text`) for inline code.\n"
+	"Infer missing context from previous messages. Never ask for clarification.\n";
+
+llama_model* model;
+llama_context* ctx;
+const llama_vocab* vocab;
+llama_sampler* smpl;
+
+int init_model() {
 	ggml_backend_load_all();
 	llama_model_params model_params = llama_model_default_params();
 	model_params.n_gpu_layers = ngl;
-	
-	llama_model* model = llama_model_load_from_file(model_path.c_str(), model_params);
-	if (model == NULL) {
-		printf("error: unable to load model");
-		return 1;
-	}
-	const llama_vocab* vocab = llama_model_get_vocab(model);
-	const int n_prompt = -llama_tokenize(vocab, prompt.c_str(), prompt.size(), NULL, 0, true, true);
 
-	std::vector<llama_token> prompt_tokens(n_prompt);
-	if (llama_tokenize(vocab, prompt.c_str(), prompt.size(), prompt_tokens.data(), prompt_tokens.size(), true, true) < 0) {
-		printf("error: failed to tokenize prompt\n");
+	model = llama_model_load_from_file(model_path.c_str(), model_params);
+	if (model == NULL) {
+		printf("error: unable to load model.\n");
 		return 1;
 	}
+
+	vocab = llama_model_get_vocab(model);
 
 	llama_context_params ctx_params = llama_context_default_params();
-	ctx_params.n_ctx = n_prompt + n_predict -1;
-	ctx_params.n_batch = n_prompt;
-	ctx_params.no_perf = false;
+	ctx_params.n_ctx = 2048;
+	ctx_params.n_batch = 512;
 
-	llama_context* ctx = llama_init_from_model(model, ctx_params);
+	ctx = llama_init_from_model(model, ctx_params);
+	if (ctx == NULL) {
+		printf("error: failed to create context.\n");
+		return 1;
+	}
 
-	    // initialize the sampler
+	auto sparams = llama_sampler_chain_default_params();
+	smpl = llama_sampler_chain_init(sparams);
 
-    auto sparams = llama_sampler_chain_default_params();
-    sparams.no_perf = false;
-    llama_sampler * smpl = llama_sampler_chain_init(sparams);
+	// match python tuning
+	//llama_sampler_chain_add(smpl, llama_sample_init_temp(0.2f));
+	// or just do greedy
+	llama_sampler_chain_add(smpl, llama_sampler_init_greedy());
+	
+	return 0;
+}
 
-    llama_sampler_chain_add(smpl, llama_sampler_init_greedy());
+std::string run_llm(const std::string& prompt) {
+	std::string final_prompt = "\nUser: " + prompt + "\nAssistant: ";
 
-    // print the prompt token-by-token
+	int n_prompt = -llama_tokenize(vocab, final_prompt.c_str(), final_prompt.size(), NULL, 0, true, true);
+	std::vector<llama_token> prompt_tokens(n_prompt);
+	if (llama_tokenize(vocab, final_prompt.c_str(), final_prompt.size(), prompt_tokens.data(), prompt_tokens.size(), true, true) < 0) {
+		// TODO: error handling
+		printf("error: failed to tokenize prompt.\n");
+		return "";
+		}
+	llama_batch batch = llama_batch_get_one(prompt_tokens.data(), prompt_tokens.size());
 
-    for (auto id : prompt_tokens) {
-        char buf[128];
-        int n = llama_token_to_piece(vocab, id, buf, sizeof(buf), 0, true);
-        if (n < 0) {
-            fprintf(stderr, "%s: error: failed to convert token to piece\n", __func__);
-            return 1;
-        }
-        std::string s(buf, n);
-        printf("%s", s.c_str());
-    }
+	std::string output;
+	llama_token new_token_id;
 
-    // prepare a batch for the prompt
+	for (int n_pos = 0; n_pos+batch.n_tokens < n_prompt + n_predict;) {
+		if (llama_decode(ctx, batch)) {
+			printf("error: llama_decode failed");
+			return "";
+		}
+	n_pos += batch.n_tokens;
+	new_token_id = llama_sampler_sample(smpl, ctx, -1);
+	if (llama_vocab_is_eog(vocab, new_token_id)) break;
 
-    llama_batch batch = llama_batch_get_one(prompt_tokens.data(), prompt_tokens.size());
+	char buf[128];
+	int n = llama_token_to_piece(vocab, new_token_id, buf, sizeof(buf), 0, true);
+	if (n < 0) continue;
 
-    // main loop
+	output.append(buf, n);
 
-    const auto t_main_start = ggml_time_us();
-    int n_decode = 0;
-    llama_token new_token_id;
+	batch = llama_batch_get_one(&new_token_id, 1);
+	}
+	return output;
+}
 
-    for (int n_pos = 0; n_pos + batch.n_tokens < n_prompt + n_predict; ) {
-        // evaluate the current batch with the transformer model
-        if (llama_decode(ctx, batch)) {
-            fprintf(stderr, "%s : failed to eval, return code %d\n", __func__, 1);
-            return 1;
-        }
-
-        n_pos += batch.n_tokens;
-
-        // sample the next token
-        {
-            new_token_id = llama_sampler_sample(smpl, ctx, -1);
-
-            // is it an end of generation?
-            if (llama_vocab_is_eog(vocab, new_token_id)) {
-                break;
-            }
-
-            char buf[128];
-            int n = llama_token_to_piece(vocab, new_token_id, buf, sizeof(buf), 0, true);
-            if (n < 0) {
-                fprintf(stderr, "%s: error: failed to convert token to piece\n", __func__);
-                return 1;
-            }
-            std::string s(buf, n);
-            printf("%s", s.c_str());
-            fflush(stdout);
-
-            // prepare the next batch with the sampled token
-            batch = llama_batch_get_one(&new_token_id, 1);
-
-            n_decode += 1;
-        }
-    }
-
-    printf("\n");
-
-    const auto t_main_end = ggml_time_us();
-
-    fprintf(stderr, "%s: decoded %d tokens in %.2f s, speed: %.2f t/s\n",
-            __func__, n_decode, (t_main_end - t_main_start) / 1000000.0f, n_decode / ((t_main_end - t_main_start) / 1000000.0f));
-
-    fprintf(stderr, "\n");
-    llama_perf_sampler_print(smpl);
-    llama_perf_context_print(ctx);
-    fprintf(stderr, "\n");
-
-    /*llama_sampler_free(smpl);
-    llama_free(ctx);
-    llama_model_free(model);*/
-
-    	
+int main() {
+    	if (init_model() == 1) {
+		return 1;
+	}
 	crow::SimpleApp app;
 
 	CROW_ROUTE(app, "/")([](){
@@ -128,8 +105,15 @@ int main() {
 		if (!body) { return crow::response(400, "Invalid JSON"); }
 
 		std::string prompt = body["prompt"].s();
+
+		std::string response = run_llm(prompt);
+		if (response == "") {
+			crow::json::wvalue err;
+			err["error"] = "error: response was empty.";
+			return crow::response(500, err.dump());
+		}
 		crow::json::wvalue res;
-		res["msg"] = run_llm(prompt); // TODO: implement method
+		res["msg"] = response;
 		return crow::response(res);
 	});
 
